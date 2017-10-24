095cb3652f9601808697136de6bce46aad371681
Gandora <gandora@bluewin.ch>
2017-10-24 19:50:39 +0200

    functional Header, even though it's not exactly what I wanted

--- js/binary.js
+++ js/binary.js
@@ -7,16 +7,12 @@ function startBinaryHeader(){
     binaryCanvas.height = rect.height;
     
     //Add custom properties
-    binaryCanvas.counterToNextNumber = 0;
-    binaryCanvas.updateTimeCounter = updateTimeCounter;
     binaryCanvas.centerTextHeight = binaryCanvas.height / 2;
     binaryCanvas.appendNewRandomNumber = appendNewRandomNumber;
     binaryCanvas.textToWrite = "";
-    binaryCanvas.updateStep = updateStep;
-    binaryCanvas.isLengthBelowMargin = isLengthBelowMargin;
     binaryCanvas.overFlowMargin = 50;
     binaryCanvas.appendNewRandomNumber();
-    
+
     //Get and setup context for drawing
     let binaryCtx = binaryCanvas.getContext('2d');
     binaryCtx.font = '35px Josefin Sans';
@@ -26,49 +22,31 @@ function startBinaryHeader(){
 }
 
 function startDrawingLoop() {
-    let interValID = setInterval(updateStep, 1000/70);
-}
-
-function updateTimeCounter(){
-    binaryCanvas.counterToNextNumber++;
-    if(binaryCanvas.counterToNextNumber === 60) {
-        binaryCanvas.counterToNextNumber = -30;
-    }
+    let interValID = setInterval(updateStep, 200);
 }
 
 function cutText(text){
-    if (text.length > 20) {
-        return text.slice(0, 20);
+    if (text.length > 128) {
+        return text.slice(0, 128);
     }
     return text;
 }
 
+function clearCanvas(context){
+    context.clearRect(0, 0, binaryCanvas.width, binaryCanvas.height);
+}
+
+function writeTextToCanvas(context){
+    context.fillText(binaryCanvas.textToWrite, 0, binaryCanvas.centerTextHeight);
+}
+
 function updateStep(){
-    binaryCanvas.updateTimeCounter();
-    if (binaryCanvas.isLengthBelowMargin()){
-        binaryCanvas.appendNewRandomNumber();
-    }
+    binaryCanvas.appendNewRandomNumber();
     binaryCanvas.textToWrite = cutText(binaryCanvas.textToWrite);
-    console.log("text to write:", binaryCanvas.textToWrite);
-    console.log("counter:", binaryCanvas.counterToNextNumber);
+    
     let binaryCtx = binaryCanvas.getContext("2d");
-    binaryCtx.font = '35px Josefin Sans';
-    binaryCtx.textBaseline = 'hanging';
-    binaryCtx.lineWidth = 3;
-    binaryCtx.fillText = (binaryCanvas.textToWrite, 200, binaryCanvas.centerTextHeight);
-}
-
-function isLengthBelowMargin(){
-    let canvasWidth = parseInt(getComputedStyle(binaryCanvas).width + binaryCanvas.overFlowMargin);
-    let textWidth = binaryCanvas.getContext("2d").measureText(binaryCanvas.textToWrite).width;
-    let result = canvasWidth > textWidth;
-    console.log(canvasWidth, textWidth, result);
-    return result;     
-}
-
-
-function isTimeToSpawnNextNumber(){
-    return binaryCanvas.counterToNextNumber === 0;
+    clearCanvas(binaryCtx);
+    writeTextToCanvas(binaryCtx);
 }
 
 
